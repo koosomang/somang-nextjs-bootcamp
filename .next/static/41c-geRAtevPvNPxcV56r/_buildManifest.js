e1444b651c906c9f00b9455f3d135e8341cf37d1
koosomang <tjddbs558@naver.com>
2021-05-28 15:21:34 +0900

    npm run build

---
+++ .next/static/41c-geRAtevPvNPxcV56r/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-e2fac8560a556168459e.js"],"/_error":["static/chunks/pages/_error-d799e3a96772416696b7.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
